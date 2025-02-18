d0470dae233b66d6b51ce48bb5305c4570c23c71
Geeky <157978295+Geekyowner@users.noreply.github.com>
2025-02-18 10:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'TechVJ'
-
+def home():
+    return "Ankit"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
